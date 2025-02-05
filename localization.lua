67c017f5c24ccae7bd9e6a7038bd2c2f86125e27
Marek Madejski <marekmadejski@yandex.com>
2025-02-05 18:39:46 +0100

    Corrected German and French ability names (per Wowhead)

--- localization.lua
+++ localization.lua
@@ -3,27 +3,26 @@
 	French, German and English (default) available
 ]]
 
-if ( GetLocale() == "deDE" ) then
-
+if (GetLocale() == "deDE") then
 	BCTM_TEXT_TITLE_BUTTON = "Einstellungen";
 	BCTM_TEXT_TOOLTIP      = "Aufsp\195\188ren";
-
+	
 	BINDING_HEADER_BCTM_BINDINGS_HEADER        = "BCUI Tracking Menu";
 	BINDING_NAME_BCTM_BINDING_TOGGLE_MENU      = "Toggle Tracking Menu";
 	BINDING_NAME_BCTM_BINDING_FIND_HERBS       = "Kr\195\164utersuche";
 	BINDING_NAME_BCTM_BINDING_FIND_MINERALS    = "Mineraliensuche";
 	BINDING_NAME_BCTM_BINDING_FIND_TREASURE    = "Schatzsucher";
-	BINDING_NAME_BCTM_BINDING_TRACK_BEASTS     = "Wildtiere aufsp\195\188ren";
-	BINDING_NAME_BCTM_BINDING_TRACK_HUMANOIDS  = "Humanoide aufsp\195\188ren";
-	BINDING_NAME_BCTM_BINDING_TRACK_HIDDEN     = "Verborgenes aufsp\195\188ren";
-	BINDING_NAME_BCTM_BINDING_TRACK_ELEMENTALS = "Elementargeister aufsp\195\188ren";
-	BINDING_NAME_BCTM_BINDING_TRACK_UNDEAD     = "Untote aufsp\195\188ren";
-	BINDING_NAME_BCTM_BINDING_TRACK_DEMONS     = "D\195\164monen aufsp\195\188ren";
-	BINDING_NAME_BCTM_BINDING_TRACK_GIANTS     = "Riesen aufsp\195\188ren";
-	BINDING_NAME_BCTM_BINDING_TRACK_DRAGONKIN  = "Drakin aufsp\195\188ren";
+	BINDING_NAME_BCTM_BINDING_SENSE_DEMONS     = "D\195\164monen sp\195\188ren";
 	BINDING_NAME_BCTM_BINDING_SENSE_UNDEAD     = "Untote Sp\195\188ren";
-	BINDING_NAME_BCTM_BINDING_SENSE_DEMONS     = "D\195\164monenersp\195\188rung";
-
+	BINDING_NAME_BCTM_BINDING_TRACK_BEASTS     = "Wildtiere aufsp\195\188ren";
+	BINDING_NAME_BCTM_BINDING_TRACK_DEMONS     = "D\195\164monen aufsp\195\188ren";
+	BINDING_NAME_BCTM_BINDING_TRACK_DRAGONKIN  = "Drachkin aufsp\195\188ren";
+	BINDING_NAME_BCTM_BINDING_TRACK_ELEMENTALS = "Elementare aufsp\195\188ren";
+	BINDING_NAME_BCTM_BINDING_TRACK_GIANTS     = "Riesen aufsp\195\188ren";
+	BINDING_NAME_BCTM_BINDING_TRACK_HIDDEN     = "Verborgenes aufsp\195\188ren";
+	BINDING_NAME_BCTM_BINDING_TRACK_HUMANOIDS  = "Humanoide aufsp\195\188ren";
+	BINDING_NAME_BCTM_BINDING_TRACK_UNDEAD     = "Untote aufsp\195\188ren";
+	
 	BCTM_TEXT_CONFIG_TITLE      = "Aufsp\195\188r-Men\195\188 Einstellungen";
 	BCTM_TEXT_SHOWONMOUSE       = "Bei Ber\195\188hrung zeigen";
 	BCTM_TEXT_HIDEONMOUSE       = "Bei Ber\195\188hrung verstecken";
@@ -40,44 +39,40 @@ if ( GetLocale() == "deDE" ) then
 	BCTM_TEXT_BLINKDURATION_TIP = "Dauer einer Ein-/Ausblenden anpassen";
 	BCTM_TEXT_BLINKDELAY        = "Blinkverz\195\182gerung";
 	BCTM_TEXT_BLINKDELAY_TIP    = "Pause zwischen dem Blinken des Icons anpassen";
-
-	BCTM_TEXT_FIND_HERBS    = "Kr\195\164utersuche";
-	BCTM_TEXT_FIND_MINERALS = "Mineraliensuche";
-	BCTM_TEXT_FIND_TREASURE = "Schatzsucher";
-
+	
+	BCTM_TEXT_FIND_HERBS       = "Kr\195\164utersuche";
+	BCTM_TEXT_FIND_MINERALS    = "Mineraliensuche";
+	BCTM_TEXT_FIND_TREASURE    = "Schatzsucher";
+	BCTM_TEXT_SENSE_DEMONS     = "D\195\164monen sp\195\188ren";
+	BCTM_TEXT_SENSE_UNDEAD     = "Untote Sp\195\188ren";
 	BCTM_TEXT_TRACK_BEASTS     = "Wildtiere aufsp\195\188ren";
-	BCTM_TEXT_TRACK_HUMANOIDS  = "Humanoide aufsp\195\188ren";
-	BCTM_TEXT_TRACK_HIDDEN     = "Verborgenes aufsp\195\188ren";
-	BCTM_TEXT_TRACK_ELEMENTALS = "Elementargeister aufsp\195\188ren";
-	BCTM_TEXT_TRACK_UNDEAD     = "Untote aufsp\195\188ren";
 	BCTM_TEXT_TRACK_DEMONS     = "D\195\164monen aufsp\195\188ren";
+	BCTM_TEXT_TRACK_DRAGONKIN  = "Drachkin aufsp\195\188ren";
+	BCTM_TEXT_TRACK_ELEMENTALS = "Elementare aufsp\195\188ren";
 	BCTM_TEXT_TRACK_GIANTS     = "Riesen aufsp\195\188ren";
-	BCTM_TEXT_TRACK_DRAGONKIN  = "Drakin aufsp\195\188ren";
-
-	BCTM_TEXT_SENSE_UNDEAD = "Untote Sp\195\188ren";
-	BCTM_TEXT_SENSE_DEMONS = "D\195\164monenersp\195\188rung";
-
-elseif ( GetLocale() == "frFR" ) then
-
+	BCTM_TEXT_TRACK_HIDDEN     = "Verborgenes aufsp\195\188ren";
+	BCTM_TEXT_TRACK_HUMANOIDS  = "Humanoide aufsp\195\188ren";
+	BCTM_TEXT_TRACK_UNDEAD     = "Untote aufsp\195\188ren";
+elseif (GetLocale() == "frFR") then
 	BCTM_TEXT_TITLE_BUTTON = "Options";
-	BCTM_TEXT_TOOLTIP    = "Choisir Pistage";
-
+	BCTM_TEXT_TOOLTIP      = "Choisir Pistage";
+	
 	BINDING_HEADER_BCTM_BINDINGS_HEADER        = "BCUI Tracking Menu";
 	BINDING_NAME_BCTM_BINDING_TOGGLE_MENU      = "Activer/D\195\169sactiver Tracking Menu";
 	BINDING_NAME_BCTM_BINDING_FIND_HERBS       = "D\195\169couverte d'herbes";
 	BINDING_NAME_BCTM_BINDING_FIND_MINERALS    = "D\195\169couverte de gisements";
 	BINDING_NAME_BCTM_BINDING_FIND_TREASURE    = "D\195\169couverte de tr\195\169sors";
+	BINDING_NAME_BCTM_BINDING_SENSE_DEMONS     = "D\195\169tection des d\195\169mons";
+	BINDING_NAME_BCTM_BINDING_SENSE_UNDEAD     = "D\195\169tection des morts-vivants";
 	BINDING_NAME_BCTM_BINDING_TRACK_BEASTS     = "Pistage des b\195\170tes";
-	BINDING_NAME_BCTM_BINDING_TRACK_HUMANOIDS  = "Pistage des humano\195\175des";
-	BINDING_NAME_BCTM_BINDING_TRACK_HIDDEN     = "Pistage des camoufl\195\169s";
-	BINDING_NAME_BCTM_BINDING_TRACK_ELEMENTALS = "Pistage des \195\169l\195\169mentaires";
-	BINDING_NAME_BCTM_BINDING_TRACK_UNDEAD     = "Pistage des morts-vivants";
 	BINDING_NAME_BCTM_BINDING_TRACK_DEMONS     = "Pistage des d\195\169mons";
-	BINDING_NAME_BCTM_BINDING_TRACK_GIANTS     = "Pistage des g\195\169ants";
 	BINDING_NAME_BCTM_BINDING_TRACK_DRAGONKIN  = "Pistage des draconiens";
-	BINDING_NAME_BCTM_BINDING_SENSE_UNDEAD     = "Sentir les morts-vivants";
-	BINDING_NAME_BCTM_BINDING_SENSE_DEMONS     = "Sentir les d\195\169mons";
-
+	BINDING_NAME_BCTM_BINDING_TRACK_ELEMENTALS = "Pistage des \195\169l\195\169mentaires";
+	BINDING_NAME_BCTM_BINDING_TRACK_GIANTS     = "Pistage des g\195\169ants";
+	BINDING_NAME_BCTM_BINDING_TRACK_HIDDEN     = "Pistage des camoufl\195\169s";
+	BINDING_NAME_BCTM_BINDING_TRACK_HUMANOIDS  = "Pistage des humano\195\175des";
+	BINDING_NAME_BCTM_BINDING_TRACK_UNDEAD     = "Pistage des morts-vivants";
+	
 	BCTM_TEXT_CONFIG_TITLE      = "Tracking Menu: Options";
 	BCTM_TEXT_SHOWONMOUSE       = "Montrer sur passage souris";
 	BCTM_TEXT_HIDEONMOUSE       = "Cacher hors passage souris";
@@ -94,44 +89,40 @@ elseif ( GetLocale() == "frFR" ) then
 	BCTM_TEXT_BLINKDURATION_TIP = "Adjusts the time it takes to fade the tracking icon in and out";
 	BCTM_TEXT_BLINKDELAY        = "Blink Delay";
 	BCTM_TEXT_BLINKDELAY_TIP    = "Adjusts the time between blinks of the icon";
-
-	BCTM_TEXT_FIND_HERBS    = "D\195\169couverte d'herbes";
-	BCTM_TEXT_FIND_MINERALS = "D\195\169couverte de gisements";
-	BCTM_TEXT_FIND_TREASURE = "D\195\169couverte de tr\195\169sors";
-
+	
+	BCTM_TEXT_FIND_HERBS       = "D\195\169couverte d'herbes";
+	BCTM_TEXT_FIND_MINERALS    = "D\195\169couverte de gisements";
+	BCTM_TEXT_FIND_TREASURE    = "D\195\169couverte de tr\195\169sors";
+	BCTM_TEXT_SENSE_DEMONS     = "D\195\169tection des d\195\169mons";
+	BCTM_TEXT_SENSE_UNDEAD     = "D\195\169tection des morts-vivants";
 	BCTM_TEXT_TRACK_BEASTS     = "Pistage des b\195\170tes";
-	BCTM_TEXT_TRACK_HUMANOIDS  = "Pistage des humano\195\175des";
-	BCTM_TEXT_TRACK_HIDDEN     = "Pistage des camoufl\195\169s";
-	BCTM_TEXT_TRACK_ELEMENTALS = "Pistage des \195\169l\195\169mentaires";
-	BCTM_TEXT_TRACK_UNDEAD     = "Pistage des morts-vivants";
 	BCTM_TEXT_TRACK_DEMONS     = "Pistage des d\195\169mons";
-	BCTM_TEXT_TRACK_GIANTS     = "Pistage des g\195\169ants";
 	BCTM_TEXT_TRACK_DRAGONKIN  = "Pistage des draconiens";
-
-	BCTM_TEXT_SENSE_UNDEAD = "Sentir les morts-vivants";
-	BCTM_TEXT_SENSE_DEMONS = "Sentir les d\195\169mons";
-
-else -- english by default
-
+	BCTM_TEXT_TRACK_ELEMENTALS = "Pistage des \195\169l\195\169mentaires"
+	BCTM_TEXT_TRACK_GIANTS     = "Pistage des g\195\169ants";
+	BCTM_TEXT_TRACK_HIDDEN     = "Pistage des camoufl\195\169s";
+	BCTM_TEXT_TRACK_HUMANOIDS  = "Pistage des humano\195\175des";
+	BCTM_TEXT_TRACK_UNDEAD     = "Pistage des morts-vivants";
+else -- English by default
 	BCTM_TEXT_TITLE_BUTTON = "Options";
 	BCTM_TEXT_TOOLTIP      = "Set Tracking";
-
+	
 	BINDING_HEADER_BCTM_BINDINGS_HEADER        = "BCUI Tracking Menu";
 	BINDING_NAME_BCTM_BINDING_TOGGLE_MENU      = "Toggle Tracking Menu";
 	BINDING_NAME_BCTM_BINDING_FIND_HERBS       = "Find Herbs";
 	BINDING_NAME_BCTM_BINDING_FIND_MINERALS    = "Find Minerals";
 	BINDING_NAME_BCTM_BINDING_FIND_TREASURE    = "Find Treasure";
-	BINDING_NAME_BCTM_BINDING_TRACK_BEASTS     = "Track Beasts";
-	BINDING_NAME_BCTM_BINDING_TRACK_HUMANOIDS  = "Track Humanoids";
-	BINDING_NAME_BCTM_BINDING_TRACK_HIDDEN     = "Track Hidden";
-	BINDING_NAME_BCTM_BINDING_TRACK_ELEMENTALS = "Track Elementals";
-	BINDING_NAME_BCTM_BINDING_TRACK_UNDEAD     = "Track Undead";
-	BINDING_NAME_BCTM_BINDING_TRACK_DEMONS     = "Track Demons";
-	BINDING_NAME_BCTM_BINDING_TRACK_GIANTS     = "Track Giants";
-	BINDING_NAME_BCTM_BINDING_TRACK_DRAGONKIN  = "Track Dragonkin";
-	BINDING_NAME_BCTM_BINDING_SENSE_UNDEAD     = "Sense Undead";
 	BINDING_NAME_BCTM_BINDING_SENSE_DEMONS     = "Sense Demons";
-
+	BINDING_NAME_BCTM_BINDING_SENSE_UNDEAD     = "Sense Undead";
+	BINDING_NAME_BCTM_BINDING_TRACK_BEASTS     = "Track Beasts";
+	BINDING_NAME_BCTM_BINDING_TRACK_DEMONS     = "Track Demons";
+	BINDING_NAME_BCTM_BINDING_TRACK_DRAGONKIN  = "Track Dragonkin";
+	BINDING_NAME_BCTM_BINDING_TRACK_ELEMENTALS = "Track Elementals";
+	BINDING_NAME_BCTM_BINDING_TRACK_GIANTS     = "Track Giants";
+	BINDING_NAME_BCTM_BINDING_TRACK_HIDDEN     = "Track Hidden";
+	BINDING_NAME_BCTM_BINDING_TRACK_HUMANOIDS  = "Track Humanoids";
+	BINDING_NAME_BCTM_BINDING_TRACK_UNDEAD     = "Track Undead";
+	
 	BCTM_TEXT_CONFIG_TITLE      = "Tracking Menu Options";
 	BCTM_TEXT_SHOWONMOUSE       = "Show on mouse over";
 	BCTM_TEXT_HIDEONMOUSE       = "Hide on mouse out";
@@ -149,20 +140,17 @@ else -- english by default
 	BCTM_TEXT_BLINKDELAY        = "Blink Delay";
 	BCTM_TEXT_BLINKDELAY_TIP    = "Adjusts the time between blinks of the icon";
 	
-	BCTM_TEXT_FIND_HERBS    = "Find Herbs";
-	BCTM_TEXT_FIND_MINERALS = "Find Minerals";
-	BCTM_TEXT_FIND_TREASURE = "Find Treasure";
-
+	BCTM_TEXT_FIND_HERBS       = "Find Herbs";
+	BCTM_TEXT_FIND_MINERALS    = "Find Minerals";
+	BCTM_TEXT_FIND_TREASURE    = "Find Treasure";
+	BCTM_TEXT_SENSE_DEMONS     = "Sense Demons";
+	BCTM_TEXT_SENSE_UNDEAD     = "Sense Undead";
 	BCTM_TEXT_TRACK_BEASTS     = "Track Beasts";
-	BCTM_TEXT_TRACK_HUMANOIDS  = "Track Humanoids";
-	BCTM_TEXT_TRACK_HIDDEN     = "Track Hidden";
-	BCTM_TEXT_TRACK_ELEMENTALS = "Track Elementals";
-	BCTM_TEXT_TRACK_UNDEAD     = "Track Undead";
 	BCTM_TEXT_TRACK_DEMONS     = "Track Demons";
-	BCTM_TEXT_TRACK_GIANTS     = "Track Giants";
 	BCTM_TEXT_TRACK_DRAGONKIN  = "Track Dragonkin";
-
-	BCTM_TEXT_SENSE_UNDEAD = "Sense Undead";
-	BCTM_TEXT_SENSE_DEMONS = "Sense Demons";
-
+	BCTM_TEXT_TRACK_ELEMENTALS = "Track Elementals";
+	BCTM_TEXT_TRACK_GIANTS     = "Track Giants";
+	BCTM_TEXT_TRACK_HIDDEN     = "Track Hidden";
+	BCTM_TEXT_TRACK_HUMANOIDS  = "Track Humanoids";
+	BCTM_TEXT_TRACK_UNDEAD     = "Track Undead";
 end
